e64752c612f8a4d2f47667e843a5dc0418c976c7
huttershq-debug <hutters.hq@gmail.com>
2026-09-05 20:42:34 +0200

    Update EmailScanWorker.kt

--- app/src/main/java/com/juaris/app/email/EmailScanWorker.kt
+++ app/src/main/java/com/juaris/app/email/EmailScanWorker.kt
@@ -1,14 +1,10 @@
-package com.juaris.app
+package com.juaris.app.email
+
+import com.juaris.app.QuantumEngine
 
 class EmailScanWorker {
     fun scanLocalEmailContent(sender: String, subject: String): Boolean {
         val hash = QuantumEngine.generateSecureHash(subject)
-        val isThreat = subject.contains("Invoice", ignoreCase = true) || subject.contains("Bank", ignoreCase = true)
-        
-        if (isThreat) {
-            JuarisEventBus.postEvent("[!] E-Mail-Bedrohung erkannt: $sender (Quantum-Hash: ${hash.take(8)})")
-            return true
-        }
-        return false
+        return subject.contains("Invoice", ignoreCase = true) || subject.contains("Bank", ignoreCase = true)
     }
 }
