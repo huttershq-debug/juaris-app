8ece62ae5fe0bccc83a719c68fa87b8e40e76447
huttershq-debug <hutters.hq@gmail.com>
2026-09-05 20:53:24 +0200

    Update EmailScanWorker.kt

--- app/src/main/java/com/juaris/app/email/EmailScanWorker.kt
+++ app/src/main/java/com/juaris/app/email/EmailScanWorker.kt
@@ -1,15 +1,9 @@
-
-Benedikt Wolfgang Hütter <hutters.hq@gmail.com>
-20:47 (vor 0 Minuten)
-an mich
-
 package com.juaris.app.email
 
 import com.juaris.app.QuantumEngine
 
 class EmailScanWorker {
     fun scanLocalEmailContent(sender: String, subject: String): Boolean {
-        // Korrekter Methodenaufruf passend zur QuantumEngine
         val hash = QuantumEngine.generateThreatSignature(subject)
         
         return subject.contains("Invoice", ignoreCase = true) || 
