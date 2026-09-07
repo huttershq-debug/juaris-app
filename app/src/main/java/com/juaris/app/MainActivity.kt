9da6792c1c01b232c2522ff1b39dbcde94638b44
huttershq-debug <hutters.hq@gmail.com>
2026-09-07 18:30:36 +0200

    Update MainActivity.kt

--- app/src/main/java/com/juaris/app/MainActivity.kt
+++ app/src/main/java/com/juaris/app/MainActivity.kt
@@ -17,7 +17,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
-import androidx.compose foundation.borderStroke
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
