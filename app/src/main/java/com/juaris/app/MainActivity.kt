47c8dbfd889076cff1bec01eb4b0095fcdba6d42
huttershq-debug <hutters.hq@gmail.com>
2026-09-11 10:31:04 +0200

    Update MainActivity.kt

--- app/src/main/java/com/juaris/app/MainActivity.kt
+++ app/src/main/java/com/juaris/app/MainActivity.kt
@@ -4,6 +4,8 @@ package com.juaris.app
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPageSate
 import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
