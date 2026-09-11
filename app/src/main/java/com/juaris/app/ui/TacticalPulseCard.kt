941d2cd7dca9ec4794c85f46aee38e98bda4b2eb
huttershq-debug <hutters.hq@gmail.com>
2026-09-11 08:27:19 +0200

    Update TacticalPulseCard.kt

--- app/src/main/java/com/juaris/app/ui/TacticalPulseCard.kt
+++ app/src/main/java/com/juaris/app/ui/TacticalPulseCard.kt
@@ -14,8 +14,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
-val NeonGiftgruen = Color(0xFF00FF66)
-
 
 @Composable
 fun TacticalPulseCard(
