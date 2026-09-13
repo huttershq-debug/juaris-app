1a0a2787be5ada96c3005ea864c271ac5a4e6068
huttershq-debug <hutters.hq@gmail.com>
2026-09-13 03:43:58 +0200

    Create MashDao.kt

---
+++ app/src/main/java/com/juaris/app/MashDao.kt
@@ -0,0 +1,18 @@
+package com.juaris.app
+
+import androidx.room.Dao
+import androidx.Room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface MeshDao {
+
+    @Query("SELECT * FROM mesh_posts ORDER BY timestamp DESC")
+    fun getAllPosts(): Flow<List<MeshPostEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertPost(post: MeshPostEntity)
+}
+
