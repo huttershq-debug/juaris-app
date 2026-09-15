ac12773487e2b9a6d328518e7014ae040e935254
huttershq-debug <hutters.hq@gmail.com>
2026-09-15 13:03:22 +0200

    Update MainActivity.kt

--- app/src/main/java/com/juaris/app/MainActivity.kt
+++ app/src/main/java/com/juaris/app/MainActivity.kt
@@ -25,6 +25,8 @@ import androidx.compose.foundation.Image
 import androidx.compose.ui.res.painterResource
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.CheckCircle
