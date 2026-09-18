9d6d271aa59a8824080c4d8caeb2241d3a31c1d2
huttershq-debug <hutters.hq@gmail.com>
2026-09-18 11:00:33 +0200

    Update MainActivity.kt

--- app/src/main/java/com/juaris/app/MainActivity.kt
+++ app/src/main/java/com/juaris/app/MainActivity.kt
@@ -179,6 +179,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    @Suppress("UnspecifiedRegisterReceiverFlag")
     private fun registerEmergencyReceiver() {
     emergencyReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
