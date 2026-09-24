21ed6c857b436562d3c2d334bb0d0821cab18ae5
huttershq-debug <hutters.hq@gmail.com>
2026-09-24 14:39:00 +0200

    Update JuarisVpnService.kt

--- app/src/main/java/com/juaris/app/JuarisVpnService.kt
+++ app/src/main/java/com/juaris/app/JuarisVpnService.kt
@@ -36,7 +36,7 @@ class JuarisVpnService : VpnService() {
         return START_STICKY
     }
 
-    private fun startForegroundServiceWithNotification() {
+     private fun startForegroundServiceWithNotification() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel = NotificationChannel(
                 CHANNEL_ID,
@@ -64,7 +64,8 @@ class JuarisVpnService : VpnService() {
             .build()
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
-            startForeground(NOTIFICATION_ID, notification, ServiceInfo.FOREGROUND_SERVICE_TYPE_VPN)
+            // Korrigiert: Android 14+ nutzt hier 'SPECIAL_USE' für VPN/Firewalls
+            startForeground(NOTIFICATION_ID, notification, ServiceInfo.FOREGROUND_SERVICE_TYPE_SPECIAL_USE)
         } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
             startForeground(NOTIFICATION_ID, notification, ServiceInfo.FOREGROUND_SERVICE_TYPE_CONNECTED_DEVICE)
         } else {
